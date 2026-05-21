ff92b944ae4da2a57f850bb7f13eb14bfc7d87ac
Ruls <soria.developer@gmail.com>
2026-05-20 22:32:21 -0600

    feature(navbarprojects): display projects name list

--- src/components/NavBarProjects.jsx
+++ src/components/NavBarProjects.jsx
@@ -1,12 +1,4 @@
-import { useState } from "react";
-
-const NavBarProjects = () => {
-  const [createProject, setCreateProject] = useState(false);
-
-  function handleClick() {
-    setCreateProject(true);
-  }
-
+const NavBarProjects = ({ createNewProject, projectsList }) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -14,12 +6,22 @@ const NavBarProjects = () => {
       </h2>
       <div>
         <button
-          onClick={handleClick}
+          onClick={createNewProject}
           className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
         >
           + Add Project
         </button>
       </div>
+      <ul className="p-4 mt-8 rounded-md ">
+        {projectsList.map((project, index) => (
+          <li
+            key={index}
+            className="flex justify-between my-4 focus:bg-stone-800"
+          >
+            {project.name}
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
