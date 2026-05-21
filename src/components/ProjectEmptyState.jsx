e66ae3466f397cc6d17289d5f52469cc30d4f37c
Ruls <soria.developer@gmail.com>
2026-05-20 22:33:03 -0600

    feature(components): include emptystate view

---
+++ src/components/ProjectEmptyState.jsx
@@ -0,0 +1,25 @@
+const ProjectEmptyState = ({ trigerNewProject }) => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src="/logo.png"
+        alt="LOGO"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-700 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 my-4">
+        Select a project or get started with a new one.
+      </p>
+      <button
+        onClick={trigerNewProject}
+        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+      >
+        Create new project
+      </button>
+    </div>
+  );
+};
+
+export default ProjectEmptyState;
