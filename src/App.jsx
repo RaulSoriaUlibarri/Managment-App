1646b01b626313bc56b01ec0840e54582887aa66
Ruls <soria.developer@gmail.com>
2026-05-20 22:29:10 -0600

    feature(app): create projects array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { NavBarProjects } from "./components";
+import { NavBarProjects, ProjectViewManager } from "./components";
+import { useState } from "react";
 
 function App() {
+  const [projects, setprojects] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+
+  function handleClick() {
+    setEditMode((prev) => !prev);
+  }
+
+  function addProject(newProject) {
+    setprojects((prev) => [...prev, newProject]);
+    console.log(projects);
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <NavBarProjects />
+        <NavBarProjects
+          createNewProject={handleClick}
+          projectsList={projects}
+        />
+        <ProjectViewManager
+          saveProject={addProject}
+          createNewProject={handleClick}
+          projectExists={editMode}
+        />
       </main>
     </>
   );
